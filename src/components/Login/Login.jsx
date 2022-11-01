d3f2116de1dbae4107c3e41344c2aeaec4401b58
KlearCj <klearmadera17@gmail.com>
2022-11-01 18:24:04 -0300

    login and signup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,6 +8,7 @@ import {
   FormLabel,
   Grid,
   GridItem,
+  Heading,
   Img,
   Input,
   Text,
@@ -21,42 +22,58 @@ const Login = () => {
   const [password, setPassword] = useState("");
 
   const isError = email || password === "";
-  const handleEmailChange = (e) =>  setEmail(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
   return (
-    <Grid templateColumns="repeat(2, 1fr)" w="100%" alignItems="center">
+    <Grid
+      templateColumns={{ md: "repeat(2, 1fr)" }}
+      w="100%"
+      alignItems="center"
+    >
       <GridItem w="100%" overflow="hidden">
-        <Img h="100vh" src={rocket} alt="welcome img" />
+        <Img h={{ base: "50%", md: "100vh" }} src={rocket} alt="welcome img" />
       </GridItem>
-      <GridItem w="100%" pl="230px">
-        <Box w="60%" >
-          <Text >Login</Text>
-          <FormControl onSubmit={()=> console.log("you're logged")}>
-            <FormLabel pt="10px">EMAIL</FormLabel>
-            <Input
-              type="email"
-              placeholder="Example@gmail.com"
-              value={email}
-              onChange={handleEmailChange}
-            />
-            <FormLabel pt="10px">PASSWORD</FormLabel>
-            <Input
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={handlePasswordChange}
+      <GridItem
+        w="100%"
+        pt={{base:"15px", md:"0"}}
+        pl={{ base: "50px", lg: "70px", xl: "120px" }}
+        pr={{ base: "50px", lg: "70px", xl: "120px" }}
+      >
+        <Flex direction="column" alignItems="center">
+          <Box w={{base:"80%",md:"60%"}} pb="20px">
+            <Heading size="lg">Welcome to UwUMovies</Heading>
+          </Box>
+          <Box w={{base:"80%",md:"60%"}}>
+            <Text fontSize="2xl">Login</Text>
+            <FormControl onSubmit={() => console.log("you're logged")}>
+              <FormLabel pt="10px">EMAIL</FormLabel>
+              <Input
+                type="email"
+                placeholder="Example@gmail.com"
+                value={email}
+                onChange={handleEmailChange}
               />
-          <Button type='submit'>Login</Button>
-          </FormControl>
-          <Flex pt="15px">
-            <Text>Don`t have an account?</Text>
-            <Link to="/sign">
-            <Button pl="10px" variant='link'>
-              SignUp
-            </Button>
-            </Link>
-          </Flex>
-        </Box>
+              <FormLabel pt="10px">PASSWORD</FormLabel>
+              <Input
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <Link to="/home">
+              <Button type="submit">Login</Button>
+              </Link>
+            </FormControl>
+            <Flex pt="15px">
+              <Text>Don`t have an account?</Text>
+              <Link to="/sign">
+                <Button pl="10px" variant="link">
+                  SignUp
+                </Button>
+              </Link>
+            </Flex>
+          </Box>
+        </Flex>
       </GridItem>
     </Grid>
   );
--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -10,55 +10,73 @@ import {
   GridItem,
   Img,
   Input,
+  Text,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import yoda from "../../assets/baby_yoda.jpg";
 
 const SignIn = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  let navigate=useNavigate()
+  let navigate = useNavigate();
 
   const handleUsernameChange = (e) => setUsername(e.target.value);
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
-  const handleSubmit=(e)=> navigate("/home")
+  const handleSubmit = (e) => navigate("/home");
 
   const isError = username || email || password === "";
   return (
-    <Grid templateColumns="repeat(2, 1fr)" w="100%" alignItems="center">
+    <Grid
+      templateColumns={{ md: "repeat(2, 1fr)" }}
+      w="100%"
+      alignItems="center"
+    >
       <GridItem w="100%" overflow="hidden">
-        <Img h="100vh" src={yoda} alt="welcome img" />
+        <Img h={{ base: "50%", md: "100vh" }} src={yoda} alt="welcome img" />
       </GridItem>
-      <GridItem w="100%" pl="230px">
-        <Box w="60%">
-          <FormControl w="100%" onSubmit={handleSubmit}>
-            <FormLabel>USERNAME</FormLabel>
-            <Input
-              type="username"
-              placeholder="Enter an username"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-            <FormLabel pt="10px">EMAIL</FormLabel>
-            <Input
-              type="email"
-              placeholder="Example@gmail.com"
-              value={email}
-              onChange={handleEmailChange}
-            />
-            <FormLabel pt="10px">PASSWORD</FormLabel>
-            <Input
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <Button type='submit' variant='ghost'>SignIn</Button>
-          </FormControl>
-        </Box>
+      <GridItem
+        w="100%"
+        pt={{ base: "15px", md: "0" }}
+        pl={{ base: "50px", lg: "70px", xl: "120px" }}
+        pr={{ base: "50px", lg: "70px", xl: "120px" }}
+      >
+        <Flex direction="column" alignItems="center">
+          <Box w={{ base: "80%", md: "60%" }}>
+            <Text fontSize="2xl">SignIn</Text>
+            <FormControl w="100%" onSubmit={handleSubmit}>
+              <FormLabel pt="10px">USERNAME</FormLabel>
+              <Input
+                type="username"
+                placeholder="Enter an username"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+              <FormLabel pt="10px">EMAIL</FormLabel>
+              <Input
+                type="email"
+                placeholder="Example@gmail.com"
+                value={email}
+                onChange={handleEmailChange}
+              />
+              <FormLabel pt="10px">PASSWORD</FormLabel>
+              <Input
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+             {/*  //CAMBIAR EL LINK POR EL NAVIGATE */}
+              <Link to="/login">
+              <Button type="submit" variant="ghost">
+                SignIn
+              </Button>
+              </Link>
+            </FormControl>
+          </Box>
+        </Flex>
       </GridItem>
     </Grid>
   );
